0d72697d9da990378fa93261c52b6496827b7d9c
Aaron Steele <nzoLogic@gmail.com>
2020-06-12 14:31:13 -0400

    add auth header middleware

--- src/index.js
+++ src/index.js
@@ -1,22 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { BrowserRouter } from "react-router-dom";
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { BrowserRouter } from 'react-router-dom'
+import { setContext } from 'apollo-link-context'
 
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { AUTH_TOKEN } from './constants';
+import './index.css'
 
-
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer: ${token}` : ''
+    }
+  }
+})
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000',
 });
 
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
